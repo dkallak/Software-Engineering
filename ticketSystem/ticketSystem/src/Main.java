75af9e707072e15e58f913c023ea74b6854ad61e
dkallak <danielkallak@hotmail.com>
2019-02-18 12:06:00 +0100

    Endret et navn
    Endret til Halden fra Fredrikstad

--- ticketSystem/ticketSystem/src/Main.java
+++ ticketSystem/ticketSystem/src/Main.java
@@ -9,7 +9,7 @@ public class Main {
         //event glommaFestivalen2 = new event ("Glommafestivalen", LocalDate.of(2019,06,15),"Fredrikstad",15000,15000,visitFredrikstad);
         //event glommaFestivalen3 = new event ("Glommafestivalen", LocalDate.of(2019,06,15),"Fredrikstad",15000,15000,visitFredrikstad);
 
-        ticket movieTicket = new ticket(glommaFestivalen,LocalDate.of(2019, 04, 15),"Fredrikstad",15,8);
+        ticket movieTicket = new ticket(glommaFestivalen,LocalDate.of(2019, 04, 15),"Halden",15,8);
 
         System.out.println(event.getEventList());
         System.out.println(movieTicket);
