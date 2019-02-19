ab27754b3f148ccc6bad9fb31aa19840e3be2872
dkallak <danielkallak@hotmail.com>
2019-02-19 13:15:04 +0100

    d
    d

--- ticketSystem/ticketSystem/src/customer.java
+++ ticketSystem/ticketSystem/src/customer.java
@@ -4,7 +4,12 @@ public class customer {
     private String lastName;
     private int age;
 
-
+    public customer(String firstName, String lastName, int age) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+    }
+}
 
     private class payment {
 
@@ -13,5 +18,15 @@ public class customer {
         private int validFrom;
         private int validTo;
         private int CVC2;
+
+        public payment(String cardName, int cardNumber, int validFrom, int validTo, int CVC2) {
+            this.cardName = cardName;
+            this.cardNumber = cardNumber;
+            this.validFrom = validFrom;
+            this.validTo = validTo;
+            this.CVC2 = CVC2;
+        }
     }
+
+
 }
