9e594a3c81d4d256608023cd351c39d3230875bc
Axel Grefslie <agrefs97@gmail.com>
2019-02-25 12:17:50 +0100

    Update Main.java

--- ticketSystem/ticketSystem/src/Main.java
+++ ticketSystem/ticketSystem/src/Main.java
@@ -33,7 +33,9 @@ public class Main {
 
         //That collection of classes seems pretty complete
         //
-        //Your customer class probably needs an array / linked list of tickets, and a payment object. Ticket shouldn't be a subclass of event, a ticket isn't a type of event. Event should probably have an array of tickets valid for that event, Organizer should probably have an array of events they manage.
+        //Your customer class probably needs an array / linked list of tickets, and a payment object. Ticket
+        // shouldn't be a subclass of event, a ticket isn't a type of event. Event should probably have an array
+        // of tickets valid for that event, Organizer should probably have an array of events they manage.
         //
         //
         //
@@ -43,7 +45,9 @@ public class Main {
         //It doesn't have to no
         //It depends on how (or if!) you need to track how many people are going to an event
         //
-        //So what I'd do is have each ticket have a unique ID. Then, an event will have an array of unique IDs of valid tickets. So to check if a ticket is valid for a specific event, all you have to do is compare the ticket's unique ID to the list of unique IDs for an event!
+        //So what I'd do is have each ticket have a unique ID. Then, an event will have an array of unique
+        // IDs of valid tickets. So to check if a ticket is valid for a specific event, all you have to do is
+        // compare the ticket's unique ID to the list of unique IDs for an event!
         //
         //So yeah, you just need to track who has what ticket :)
     }
