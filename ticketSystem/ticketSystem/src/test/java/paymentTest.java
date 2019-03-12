8a96593375d4a6c58109667a091fd64372cafdb2
dkallak <danielkallak@hotmail.com>
2019-03-12 12:48:14 +0100

    Opprettet paymentTest og lagde selve testen

---
+++ ticketSystem/ticketSystem/src/test/java/paymentTest.java
@@ -0,0 +1,30 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.time.LocalDate;
+
+public class paymentTest {
+
+    @Test
+    public void doesBalanceChange() {
+        // Checks wether the customer does have enough money to pay for a ticket.
+        // If he does, he will buy the ticket. If not, he won't be able to buy it
+
+
+        organizer denNorskeTurforeningen = new organizer("Den norske turforeningen", "Henning", "Kvitnes");
+
+
+        event tripToNordby = new event("Tur til Nordby", LocalDate.of(2019,06,15), "Nordby", 70, 70, denNorskeTurforeningen, 90);
+
+        ticket billett1Nordby = new ticket(tripToNordby);
+
+        payment balanceOfJahn = new payment("Jahn Teigen", 4112, 2015, 2025, 123, 5000);
+        
+        customer jahnTeigen = new customer("Jahn", "Teigen", 69, balanceOfJahn);
+
+        jahnTeigen.addTicket(billett1Nordby, tripToNordby);
+
+        Assert.assertEquals(jahnTeigen.getCurrentPayment().getBalance(), 4910);
+    }
+
+}
