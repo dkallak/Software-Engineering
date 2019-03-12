17a3d7197c6675acfadc301ff3783816f59b46f9
Axel Grefslie <agrefs97@gmail.com>
2019-03-12 15:09:59 +0100

    Removed spacing
    removed spacing from organizerTest, eventTest, paymentTest

--- ticketSystem/ticketSystem/src/test/java/eventTest.java
+++ ticketSystem/ticketSystem/src/test/java/eventTest.java
@@ -6,15 +6,10 @@ import java.time.LocalDate;
 public class eventTest {
 
     organizer studentParlamentet = new organizer("Studentparlamentet");
-
     event tripToIbiza = new event("Trip to Ibiza", LocalDate.of(2019, 06, 12), "Ibiza", 400, 400, studentParlamentet, 3400);
-
     cinema drammenKino = new cinema("Nordic films", "Kjell", "Eriksen", "Drammen Kino", 300000);
-
     payment magnePayment = new payment("Magne Samuelsen", 7111, 2017, 2016, 566, 4400);
-
     customer magne = new customer("Magne", "Samuelsen",52, magnePayment);
-
     ticket ibizaTicket1 = new ticket(tripToIbiza, 2, 14, magne);
 
     @Test
