2706c8ac7a766edcc1ddb5b15816d6d893c64f79
dkallak <danielkallak@hotmail.com>
2019-03-18 15:25:54 +0100

    Update Main.java

--- ticketSystem/ticketSystem/src/main/java/Main.java
+++ ticketSystem/ticketSystem/src/main/java/Main.java
@@ -6,7 +6,7 @@ import java.util.Date;
 import java.util.Scanner;
 
 public class Main {
-//nfdsofndsklfdsnokfds
+
     public static void main(String[] args) {
         //Oppretter arragører
         organizer askimKino = new organizer("askimKino", "Gunnhild", "Hansen");
